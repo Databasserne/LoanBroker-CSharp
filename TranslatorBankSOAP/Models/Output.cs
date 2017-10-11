5a3f7457779d371c364e893a4e39071cbc577c36
Unknown <ale.steen@hotmail.com>
2017-10-11 11:19:29 +0200

    Changed translator output

--- TranslatorBankSOAP/Models/Output.cs
+++ TranslatorBankSOAP/Models/Output.cs
@@ -8,29 +8,20 @@ namespace TranslatorBankSOAP.Models
 {
     public class Output
     {
-        public Output(Input input, double rate)
+        public Output(Input input, double intrestRate)
         {
             this.SSN = input.SSN;
-            this.Months = input.Months;
-            this.Amount = input.Amount;
-            this.CreditScore = input.CreditScore;
-            this.Rate = rate;
+            this.IntrestRate = intrestRate;
         }
 
         // ReSharper disable once InconsistentNaming
         public string SSN { get; set; }
 
-        public int Months { get; set; }
-
-        public double Amount { get; set; }
-
-        public int CreditScore { get; set; }
-
-        public double Rate { get; set; }
+        public double IntrestRate { get; set; }
 
         public override string ToString()
         {
-            return $"SSN: {SSN} - Months: {Months} - Amount: {Amount} - CreditScore: {CreditScore} - Rate: {Rate}";
+            return $"SSN: {SSN} - Rate: {IntrestRate}";
         }
     }
 }
